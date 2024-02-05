2951d2d8d087c87d0ecc9be3fa0bc2922a62edd7
f-ssemwanga <dssales@gmx.com>
2024-02-05 07:48:51 +0000

    Added database manipulation methods

--- supportModules.py
+++ supportModules.py
@@ -19,11 +19,42 @@ def draw_text(surf, text, size, x, y, clr):
 def dbConnector():
     """Connect to the database and return a connection object"""
     try:
-        conn = sq.connect("")
+        conn = sq.connect("dbases/pygameScores.db")
         cur = conn.cursor()
         return conn, cur
     except sq.Error as e:
         print(f"Database connection error: {e}")
 
 
-dbConnector()
+def writeToDatabase():
+    """Writes to the database"""
+    conn, cur = dbConnector()  # connect to the database
+
+    query = """INSERT INTO tblScores VALUES(2, "Gio", 2,0,0)"""
+    cur.execute(query)
+    conn.commit()
+    conn.close()
+    print("Success")
+
+
+def readDatabaseRecords():
+    """Reads records from a database"""
+    conn, cur = dbConnector()
+    query = """SELECT * FROM tblScores"""
+    cur.execute(query)
+    results = cur.fetchall()
+    print(results)
+    conn.close()
+
+
+def writeToSpecific():
+    """Writes a value to a specific record"""
+    # query record to be written to
+    # prepare data to be written
+    # open connection, write data, close connection
+
+
+# test dbConnection works
+print(dbConnector())
+# writeToDatabase()
+readDatabaseRecords()
